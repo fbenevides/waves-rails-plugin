3f9bc28dc5e6e476afdb35827194881585aa9595
fbenevides <felipe.benevidesv@gmail.com>
2014-10-30 10:21:09 -0300

    again...

--- lib/wapi/parsers/info.rb
+++ lib/wapi/parsers/info.rb
@@ -1,13 +1,13 @@
 module Wapi
   module InfoParser
-	  module ClassMethods
-		  def content_for html, where
-			  html.css("#content .info div")[where].content.strip.split(":")[1].strip
-			end
-		end
+    module ClassMethods
+      def content_for html, where
+        html.css("#content .info div")[where].content.strip.split(":")[1].strip
+      end
+    end
 
-		def self.included(base)
-		  base.extend(ClassMethods)
-		end
-	end
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+  end
 end
